f60083b194d4a1760d6dd97f57173613a84b18d6
Vinix <129098852+Selesteiro@users.noreply.github.com>
2026-04-29 14:01:40 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,6 @@
+<h1>Novo Contato</h1>
+<form action="inserir.php" method="POST">
+    Nome: <input type="text" name="nome"><br>
+    Telefone: <input type="text" name="telefone"><br>
+    <input type="submit" value="Salvar">
+</form>
